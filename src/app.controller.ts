90e8e75c4ad058b2fea2672fefa13349effe58e9
Petrik Diak <diak@petrik.hu>
2024-02-13 14:31:28 +0100

    Kapcsolatok megalkotása

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,50 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly db: PrismaService
+    ) {}
 
   @Get()
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
   }
+
+  @Get('projects')
+  listPojects(){
+    return this.db.project.findMany({
+      include: {
+        leader: true
+      }
+    });
+  }
+
+
+
+  /*@Get('projects/:id/leader')
+   async getLeader(@Param('id') projectId: string){
+    const project = await this.db.project.findUniqueOrThrow({
+      where: {id:parseInt(projectId)}
+    })
+    
+  }*/
+
+
+  @Get('projectswithnames')
+  listProjectWithName(){
+    return this.db.project.findMany({
+      select:{
+        title: true,
+        leader:{ select: {name:true,salary:true}
+        }
+      }
+    })
+  }
+
+
 }
